8290ba64246b9a61a8bc2568cf094ce143ca29e2
isus203 <128573607+isus203@users.noreply.github.com>
2024-08-05 08:04:32 +0300

    Update and rename lineage_FULL-64.mk to lineage_KJ5.mk

--- lineage_KJ5.mk
+++ lineage_KJ5.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from FULL-64 device
-$(call inherit-product, device/tecno/FULL-64/device.mk)
+$(call inherit-product, device/tecno/KJ5/device.mk)
 
-PRODUCT_DEVICE := FULL-64
-PRODUCT_NAME := lineage_FULL-64
+PRODUCT_DEVICE := KJ5
+PRODUCT_NAME := lineage_KJ5
 PRODUCT_BRAND := TECNO
 PRODUCT_MANUFACTURER := tecno
 
@@ -24,4 +24,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-tecno
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sys_tssi_64_tecno-user 13 TP1A.220624.014 629930 release-keys"
 
-BUILD_FINGERPRINT := TECNO/TSSI/FULL-64:13/TP1A.220624.014/240606V1056:user/release-keys
+BUILD_FINGERPRINT := TECNO/KJ5-OP/TECNO-KJ5:12/SP1A.210812.016/240607V1280:user/release-keys

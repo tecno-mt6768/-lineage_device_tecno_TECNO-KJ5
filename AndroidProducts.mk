064dcd0f20032562274f5d68e60a3852228441e4
isus203 <128573607+isus203@users.noreply.github.com>
2024-08-05 08:00:56 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_FULL-64.mk
+    $(LOCAL_DIR)/lineage_KJ5.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_FULL-64-user \
-    lineage_FULL-64-userdebug \
-    lineage_FULL-64-eng
+    lineage_KJ5-user \
+    lineage_KJ5-userdebug \
+    lineage_KJ5-eng
